edec1122ed41c114da2c372096109785b466f3c8
ShakibulDeveloper <epiktheme@gmail.com>
2021-02-21 23:14:53 +0600

    Dashboard

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -7,13 +7,13 @@
         <meta name="description" content="HealthMeter - Your Personal Health Checker Application" />
       <title>HealthMeter - dashboard </title>
       <!--font-awesome icons link-->
-      <link rel="stylesheet" href="{{  asset('frontend_assets/css/font-awesome.min.css') }}">
+      <link rel="stylesheet" href="{{  asset('public/frontend_assets/css/font-awesome.min.css') }}">
 
       <!-- Bootstrap CSS -->
-      <link rel="stylesheet" href="{{  asset('frontend_assets/css/bootstrap.min.css') }}">
+      <link rel="stylesheet" href="{{  asset('public/frontend_assets/css/bootstrap.min.css') }}">
       <!--main style file-->
-      <link rel="stylesheet" href="{{  asset('frontend_assets/css/style.css') }}">
-      <link rel="stylesheet" href="{{  asset('frontend_assets/css/responsive.css') }}">
+      <link rel="stylesheet" href="{{  asset('public/frontend_assets/css/style.css') }}">
+      <link rel="stylesheet" href="{{  asset('public/frontend_assets/css/responsive.css') }}">
 </head>
 
 <body id="index2">
@@ -21,9 +21,9 @@
      @yield('frontend_section')
 
     <!-- Optional JavaScript -->
-    <script src="{{  asset('frontend_assets/js/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{  asset('frontend_assets/js/bootstrap.min.js')}}"></script>
-    <script src="{{  asset('frontend_assets/js/custom.js')}}"></script>
+    <script src="{{  asset('public/frontend_assets/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{  asset('public/frontend_assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{  asset('public/frontend_assets/js/custom.js')}}"></script>
 </body>
 
 </html>
