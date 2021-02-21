8066d550c19249c8ae4a91d4ec12094b4de69015
ShakibulDeveloper <epiktheme@gmail.com>
2021-02-21 23:11:49 +0600

    route

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::post('dashboard','DashboardController@dashboard')->name('dashboard');
-Route::get('dashboard','DashboardController@ReturnHome');
-Route::get('notice','DashboardController@notice');
-Route::get('about','DashboardController@about');
-Route::get('chat','DashboardController@chat');
+Route::post('/dashboard','DashboardController@dashboard')->name('dashboard');
+Route::get('/dashboard','DashboardController@ReturnHome');
+// Route::get('notice','DashboardController@notice');
+// Route::get('about','DashboardController@about');
+// Route::get('chat','DashboardController@chat');
